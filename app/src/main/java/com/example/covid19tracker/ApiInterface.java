4178e4ea038024cf236e477f3f58b46e5ef53570
SultanFaiji <ansarisultan6768@gmail.com>
2021-07-01 16:37:31 +0530

    first commit

---
+++ app/src/main/java/com/example/covid19tracker/ApiInterface.java
@@ -0,0 +1,11 @@
+package com.example.covid19tracker;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+public interface ApiInterface {
+    String BASE_URL = "https://corona.lmao.ninja/v2/";
+    @GET("countries")
+    Call<List<ModelClass>> getcountrydata();
+}
